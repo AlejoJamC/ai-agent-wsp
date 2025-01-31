d3b3d39a595d228f541906777b580341719317c0
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-01-31 22:19:07 +0100

    initial spring project, Spring Web Ollama AI PostgreSQL Driver SQL PGvector Vector Database AI Docker Compose Support Developer Tools Oracle Driver SQL Spring Data JDBC SQL Kotlin

---
+++ src/main/kotlin/com/alejojamc/ai_agent_wsp/AiAgentWspApplication.kt
@@ -0,0 +1,11 @@
+package com.alejojamc.ai_agent_wsp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiAgentWspApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiAgentWspApplication>(*args)
+}
